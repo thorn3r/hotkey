a674a1242df772b1f6958cce0287e12902612844
Tim Horner <timothy.horner@isovalent.com>
2022-11-29 15:35:28 -0500

    use flatpak list instead of flatpak history

--- hotkey.sh
+++ hotkey.sh
@@ -5,6 +5,6 @@ if [ "$(pgrep "${APP}")" ]; then
     wmctrl -a "${APP}"
 elif [ "$(which "${APP}")" ]; then
     ${1}
-elif [ "$(flatpak history | grep "${APP}")" ]; then
+elif [ "$(flatpak list | grep "${APP}")" ]; then
     flatpak run "$(flatpak list | grep "${APP}" | awk '{ print $2 }')"
 fi
